25da7af6b4fa02a99a1ec13b6f299618c8780c34
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-30 23:58:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,5 +14,128 @@ class TicTacToe
     @board = board
   end
   
+  def input_to_index(input)
+  index = input.to_i - 1
+  return index
+end
+
+def move(board, index, token)
+  board[index] = token
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  if(index.between?(0, 8) && !position_taken?(board, index))
+    return true
+  else
+    return false
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
   
+  token = current_player(board)
+  
+  if(valid_move?(board, index))
+    move(board, index, token)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |space|
+    if (space == " " || space == "" || space == nil)
+      nil
+    else
+      count += 1
+    end
+  end
+  
+  return count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  if( count.even? )
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def won?(board)
+  if( board.all?{|i| i == " "} )
+    false
+  else
+    WIN_COMBINATIONS.each do |combo|
+      if( (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X") ||
+          (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O") )
+        return combo
+      end
+    end
+    false
+  end
+end
+
+def full?(board)
+  board.all?{|i| i == "X" || i == "O" }
+end
+
+def draw?(board)
+  if( full?(board) == true && won?(board) == false )
+    return true
+  else
+    false
+  end
+end
+
+def over?(board)
+<<<<<<< HEAD
+  if(draw?(board) == true || (won?(board)))
+=======
+  if(draw?(board) == true || (won?(board))
+>>>>>>> 250a1e0cfae120cf67e93726d3c1afb2499c191e
+    return true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if(draw?(board) == over?(board))
+    nil
+  else
+    index = won?(board)
+    if(board[index[0]] == "X" && board[index[1]] == "X" && board[index[2]] == "X")
+      return "X"
+    elsif(board[index[0]] == "O" && board[index[1]] == "O" && board[index[2]] == "O")
+      return "O"
+    end
+  end
+end
+
+  def play(board)
+  <<<<<<< HEAD
+    until (over?(board))
+  =======
+    until (over?(board)==true)
+  >>>>>>> 250a1e0cfae120cf67e93726d3c1afb2499c191e
+      turn(board)
+    end
+    
+    if(won?(board))
+      puts "Congratulations #{winner(board)}!"
+    elsif(draw?(board))
+      puts "Cat's Game!"
+    end
+  end
 end
