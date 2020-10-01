c15643c742e1d7cb89e67250493370f2033be32a
Joma Pormentilla <jomapormentilla@gmail.com>
2020-10-01 00:06:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,7 +48,7 @@ class TicTacToe
     input = gets.strip
     index = input_to_index(input)
     
-    token = current_player(board)
+    token = current_player
     
     if(valid_move?(index))
       move(index, token)
