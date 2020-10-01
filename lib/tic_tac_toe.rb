a07a8ef10724459fa2d8a0851b4a97644c49f751
Joma Pormentilla <jomapormentilla@gmail.com>
2020-10-01 00:00:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,6 +14,14 @@ class TicTacToe
     @board = board
   end
   
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
   def input_to_index(input)
     index = input.to_i - 1
     return index
